ad7df807484a9644e01bbf6a40e869b744190b69
zlatieva-anastasiia-dev <159052978+zlatieva-anastasiia-dev@users.noreply.github.com>
2025-12-14 16:28:52 +0100

    feat: restructure component organization

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
-    return (
-        <header
-            className="sticky top-4 z-50 
+	return (
+		<header
+			className="sticky top-0 z-50 
             mx-4 
             h-16 
             bg-gray-900
@@ -12,18 +12,18 @@ export function Header() {
             rounded-full 
             flex items-center justify-between
             px-6 md:px-12"
-        >
-            <nav className="flex w-full items-center ">
-                <Link href="/" className="flex items-center gap-2">
-                    <Image
-                        src="/logo-white.svg"
-                        alt="Luxonis Logo"
-                        width={120}
-                        height={32}
-                        className="object-contain"
-                    />
-                </Link>
-            </nav>
-        </header>
-    );
+		>
+			<nav className="flex w-full items-center ">
+				<Link href="/" className="flex items-center gap-2">
+					<Image
+						src="/logo-white.svg"
+						alt="Luxonis Logo"
+						width={120}
+						height={32}
+						className="object-contain"
+					/>
+				</Link>
+			</nav>
+		</header>
+	);
 }
